3c335abaf46dc77dd534fd3c5f54d4d17ca684f9
Pramurta <pramurta.chatterjee@gmail.com>
2024-03-29 13:29:32 +0800

    Remove unused variable

--- src/test/java/com/pramurta/movie/controllers/AvailableShowIntegrationTests.java
+++ src/test/java/com/pramurta/movie/controllers/AvailableShowIntegrationTests.java
@@ -25,7 +25,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Optional;
+
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
